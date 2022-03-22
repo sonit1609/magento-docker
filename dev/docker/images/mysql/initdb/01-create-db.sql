ff5d588ddda73d29f0b6301b7e6b58b89b693749
SonWiki <son.nguyen@wiki-solution.com>
2022-03-22 14:38:19 +0700

    Update docker

--- dev/docker/images/mysql/initdb/01-create-db.sql
+++
@@ -1,7 +0,0 @@
-# Create Databases
-CREATE DATABASE IF NOT EXISTS `magento_db`;
-
-# Create user and grant rights
-FLUSH PRIVILEGES;
-CREATE USER 'magento'@'%' IDENTIFIED BY 'secret';
-GRANT ALL ON magento_db.* TO 'magento'@'%';
